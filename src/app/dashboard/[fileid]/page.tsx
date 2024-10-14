1dfd3d648771669517b90504e1d2946d3826660d
ImmortalSul <157292013+ImmortalSul@users.noreply.github.com>
2024-10-14 21:16:34 +0530

    Added Better Dashboard

--- src/app/dashboard/[fileid]/page.tsx
+++ src/app/dashboard/[fileid]/page.tsx
@@ -1,3 +1,5 @@
+import ChatWrapper from "@/components/ChatWrapper";
+import PdfRenderer from "@/components/PdfRenderer";
 import { db } from "@/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { notFound } from "next/navigation";
@@ -28,7 +30,20 @@ const Page = async ({ params }: PageProps) => {
 
     if(!file) notFound()
 
-    return (<div>File ID: {fileid}</div>);
+    return <div className="flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)]">
+        <div className="mx-auto w-full max-w-8xl grow lg:flex xl:px-2">
+            {/* left lol */}
+            <div className="flex-1 xl:flex">
+                <div className="px-4 py-6 sm:px-6 g:pl-8 xl:flex-1 xl:pl-6">
+                    <PdfRenderer />
+                </div>
+            </div>
+
+            <div className="shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0">
+                <ChatWrapper />
+            </div>
+        </div>
+    </div>
 }
 
 export default Page;
--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -4,7 +4,33 @@
 import { useState } from "react";
 import { Button } from "./button";
 import { DialogContent, Dialog, DialogTrigger } from "./ui/dialog";
+import Dropzone from "react-dropzone"
+import { Cloud } from "lucide-react";
 
+const UploadDropzone = () => {
+    return (
+         <Dropzone multiple={false} onDrop={(acceptedFile)=> {
+        console.log(acceptedFile)
+    }}>
+    {({getRootProps, getInputProps, acceptedFiles}) => (
+        <div{...getRootProps()} className="border h-64 m-4 border-dashed border-gray-300 rounded-lg">
+        <div className="flex items-center justify-center h-full w-full">
+            <label 
+            htmlFor="dropzone-file"
+            className="flex flex-col items-center justify-center w-full h-full rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100">
+                <div className="flex flex-col items-center justify-center pt=6 pb-6">
+                    <Cloud className="h-6 w-6 text-zinc-500 mb-2"/>
+                    <p className="mb-2 text-sm text-zinc-700">
+                        <span className="font-semibold">Click to Upload</span>{' '}or drag and drop
+                    </p>
+                </div>
+            </label>
+        </div>
+        </div>
+    )}
+    </Dropzone>
+    )
+}
 
 const UploadButton = () => {
 
@@ -22,7 +48,7 @@ const UploadButton = () => {
                 </Button>
             </DialogTrigger>
             <DialogContent>
-                fill shit here after
+                <UploadDropzone />
             </DialogContent>
         </Dialog>
     )
